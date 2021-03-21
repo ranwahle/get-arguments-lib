482262e5ea07fe179c47bbba2aef8fef01a4cd4b
Ran <ran.wahle@gmail.com>
2021-03-21 12:40:43 +0200

    adding .gitignore & type declaration file

---
+++ get-arguments.d.ts
@@ -0,0 +1,6 @@
+/**
+* Gets argument as switches with double dash, and return a dictionary object
+* @param argumentsArray - arguments array (as received from porcess.argv)
+* @return a key value object with seitchname and switch value
+*/
+function getArguments(argumentsArray: string[]);
