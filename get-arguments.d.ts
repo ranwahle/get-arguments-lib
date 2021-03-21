b42911a9ab15f6e951f75e46b0f0a697c0a054c0
Ran <ran.wahle@gmail.com>
2021-03-21 12:43:12 +0200

    Add reference path

--- get-arguments.d.ts
+++ get-arguments.d.ts
@@ -1,3 +1,6 @@
+/// <reference types="typescript" />
+
+
 /**
 * Gets argument as switches with double dash, and return a dictionary object
 * @param argumentsArray - arguments array (as received from porcess.argv)
